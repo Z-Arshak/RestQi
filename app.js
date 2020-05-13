8d06d88272d33f666ef5a2fdb6f87d721a15636d
Zakaryan <arshak.zakaryan@effem.com>
2020-05-13 23:02:53 +0200

    Commit One

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 
+
 //for handling POST requests
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
 const app = express();
 
+const todoController = require('./controller/todoController');
+
 //const path = require('path');
+
 
 app.set('view engine', 'ejs');
 
@@ -15,8 +19,15 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false })
 //for normal html file  with sendFile
 //app.get(['/', '/home', '/index'], (req, res) => res.sendFile(__dirname + '/index.html'));
 
+//fire cntrollers
+todoController(app);
+
+//static files
 //vor asset folder-i meji faylerin karoxanam kpnem
-app.use('/assets', express.static('assets'));
+//app.use('/assets', express.static('assets'));
+app.use(express.static('./assets'));
+
+
 
 //not to allow external users digg into my files
 app.get('/assets/*', function (req, res) {
@@ -36,12 +47,13 @@ app.get('/contact', function (req, res) {
     res.render('contact')
 });
 
+
 // POST /login gets urlencoded bodies
 app.post('/contact', urlencodedParser, function (req, res) {
     //res.send('welcome, ' + req.body.username)
     console.log(req.body);
     console.log(req.body.firstName);
-    res.render('contact', { data: req.body });
+    res.render('verify', { data: req.body });
 });
 
 
@@ -57,7 +69,6 @@ app.get(['/blog', '/blog/:name'], function (req, res) {
 });
 
 
-
 //in case the page does not exists. This should be at the end
 app.get('*', function (req, res) {
     res.render('404');
@@ -65,4 +76,6 @@ app.get('*', function (req, res) {
 
 
 
-app.listen(3000);
+
+// use port 3000 unless there exists a preconfigured port
+app.listen(process.env.PORT || 3000);
---
+++ controller/todoController.js
@@ -0,0 +1,56 @@
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+
+//var blogContent = [{ item: 'Content1' }, { item: 'Content2' }, { item: 'Content3' }, { item: 'Content4' }];
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+
+//connect to the database
+//mongodb://<dbuser>:<dbpassword>@ds139722.mlab.com:39722/restqi
+mongoose.connect("mongodb://user:123456Az@ds139722.mlab.com:39722/restqi", { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+var todoSchema = new mongoose.Schema({
+    item: String
+});
+
+var Todo = mongoose.model('blogContent', todoSchema);
+
+// var itemOne = Todo({ item: 'blog content1' }).save(function (err) {
+//     if (err) throw err;
+//     console.log('content is saved')
+// });
+
+
+module.exports = function (app) {
+
+    app.get('/admin', function (req, res) {
+        //to find specific item from db
+        //Todo.find({item:'buy flowers'})
+
+        //to return all itme
+        Todo.find({}, function (err, data) {
+            if (err) throw err;
+            res.render('admin', { blogContent: data });
+
+        });
+    });
+
+
+
+    app.post('/admin', urlencodedParser, function (req, res) {
+        var saveContent = Todo(req.body).save(function (err, data) {
+            if (err) throw err;
+            res.json(data);
+        });
+    });
+
+
+
+    app.delete('/admin', function (req, res) {
+
+    });
+
+};
